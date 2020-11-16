65ef65608cdc90b56af2c5613ad323a2c8285897
liuyinz <cheunghsu@outlook.com>
2020-11-17 07:32:20 +0800

    refactor : update sh formats

--- zsh/func.sh
+++ zsh/func.sh
@@ -4,42 +4,42 @@
 # -----------------------
 # [P]ip [I]nstall
 ppi() {
-	local inst
-	inst=$(curl -s "$(pip3 config get global.index-url)/" |
-		grep '</a>' | sed 's/^.*">//g' | sed 's/<.*$//g' |
-		eval "fzf ${FZF_DEFAULT_OPTS} --exact --header='[pip:install]'")
+  local inst
+  inst=$(curl -s "$(pip3 config get global.index-url)/" |
+    grep '</a>' | sed 's/^.*">//g' | sed 's/<.*$//g' |
+    eval "fzf ${FZF_DEFAULT_OPTS} --exact --header='[pip:install]'")
 
-	if [[ $inst ]]; then
-		for prog in $(echo "$inst"); do
-			pip3 install --upgrade "$prog"
-		done
-	fi
+  if [[ $inst ]]; then
+    for prog in $(echo "$inst"); do
+      pip3 install --upgrade "$prog"
+    done
+  fi
 }
 
 # [P]ip [U]pgrade
 ppu() {
-	local inst
-	inst=$(pip3 list --outdated | tail -n +3 | awk '{print $1}' |
-		eval "fzf ${FZF_DEFAULT_OPTS} --header='[pip:upgrade]'")
+  local inst
+  inst=$(pip3 list --outdated | tail -n +3 | awk '{print $1}' |
+    eval "fzf ${FZF_DEFAULT_OPTS} --header='[pip:upgrade]'")
 
-	if [[ $inst ]]; then
-		for prog in $(echo "$inst"); do
-			pip3 install --upgrade "$prog"
-		done
-	fi
+  if [[ $inst ]]; then
+    for prog in $(echo "$inst"); do
+      pip3 install --upgrade "$prog"
+    done
+  fi
 }
 
 # [P]ip [C]lean
 ppc() {
-	local inst
-	inst=$(pip3 list | tail -n +3 | awk '{print $1}' |
-		eval "fzf ${FZF_DEFAULT_OPTS} --header='[pip:uninstall]'")
+  local inst
+  inst=$(pip3 list | tail -n +3 | awk '{print $1}' |
+    eval "fzf ${FZF_DEFAULT_OPTS} --header='[pip:uninstall]'")
 
-	if [[ $inst ]]; then
-		for prog in $(echo "$inst"); do
-			pip3 uninstall --yes "$prog"
-		done
-	fi
+  if [[ $inst ]]; then
+    for prog in $(echo "$inst"); do
+      pip3 uninstall --yes "$prog"
+    done
+  fi
 }
 
 # BREW
@@ -47,114 +47,114 @@ ppc() {
 
 # [B]rew [I]nstall [P]lugin
 bip() {
-	local inst
-	inst=$(brew search | eval "fzf ${FZF_DEFAULT_OPTS} \
-        --exact --header='[brew:install]'")
+  local inst
+  inst=$(brew search | eval "fzf ${FZF_DEFAULT_OPTS} \
+    --exact --header='[brew:install]'")
 
-	if [[ $inst ]]; then
-		for prog in $(echo "$inst"); do
-			brew install "$prog"
-		done
-	fi
-
+  if [[ $inst ]]; then
+    for prog in $(echo "$inst"); do
+      [[ $(command -v -- "$prog") ]] || brew install "$prog"
+    done
+  fi
 }
 
 # [B]rew [C]ask [I]nstall [P]lugin
 bci() {
-	local inst
-	inst=$(brew search --casks | eval "fzf ${FZF_DEFAULT_OPTS} \
-        --exact --header='[brew cask:install]'")
+  local inst
+  inst=$(brew search --casks | eval "fzf ${FZF_DEFAULT_OPTS} \
+    --exact --header='[brew cask:install]'")
 
-	if [[ $inst ]]; then
-		for prog in $(echo "$inst"); do
-			brew cask install "$prog"
-		done
-	fi
+  if [[ $inst ]]; then
+    for prog in $(echo "$inst"); do
+      brew cask install "$prog"
+    done
+  fi
 }
 
 # [B]rew [C]lean [P]lugin
 bcp() {
-	local uninst
-	uninst=$(brew leaves | eval "fzf ${FZF_DEFAULT_OPTS} \
-        --header='[brew formulae:uninstall]'")
+  local uninst
+  uninst=$(brew leaves | eval "fzf ${FZF_DEFAULT_OPTS} \
+    --header='[brew formulae:uninstall]'")
 
-	if [[ $uninst ]]; then
-		for prog in $(echo "$uninst"); do
-			brew uninstall "$prog"
-		done
-	fi
+  if [[ $uninst ]]; then
+    for prog in $(echo "$uninst"); do
+      brew uninstall "$prog"
+    done
+  fi
 }
 
 # [B]rew [C]ask [C]lean [P]lugin
 bcc() {
-	local uninst
-	uninst=$(brew list --cask | eval "fzf ${FZF_DEFAULT_OPTS} \
-        --header='[brew cask:uninstall]'")
+  local uninst
+  uninst=$(brew list --cask | eval "fzf ${FZF_DEFAULT_OPTS} \
+    --header='[brew cask:uninstall]'")
 
-	if [[ $uninst ]]; then
-		for prog in $(echo "$uninst"); do
-			brew cask uninstall "$prog"
-		done
-	fi
+  if [[ $uninst ]]; then
+    for prog in $(echo "$uninst"); do
+      brew cask uninstall "$prog"
+    done
+  fi
 }
 
 # [B]rew [U]pdate [P]lugin
 bup() {
-	brew update
-	local upd
-	upd=$(brew outdated --greedy | eval "fzf ${FZF_DEFAULT_OPTS} \
-        --header='[brew:update]'")
+  brew update
+  local upd
+  upd=$(brew outdated --greedy | eval "fzf ${FZF_DEFAULT_OPTS} \
+    --header='[brew:update]'")
 
-	if [[ $upd ]]; then
-		for prog in $(echo "$upd"); do
-			brew upgrade --greedy "$prog"
-		done
-	fi
+  if [[ $upd ]]; then
+    for prog in $(echo "$upd"); do
+      brew upgrade --greedy "$prog"
+    done
+  fi
 }
 
 # [B]rew [U]n[T]ap
 but() {
-	local upd
-	upd=$(brew tap | eval "fzf ${FZF_DEFAULT_OPTS} \
-        --header='[brew untap:]'")
+  local upd
+  upd=$(brew tap | eval "fzf ${FZF_DEFAULT_OPTS} \
+    --header='[brew untap:]'")
 
-	if [[ $upd ]]; then
-		for prog in $(echo "$upd"); do
-			brew untap "$prog"
-		done
-	fi
+  if [[ $upd ]]; then
+    for prog in $(echo "$upd"); do
+      brew untap "$prog"
+    done
+  fi
 }
+
 # PATH
 # ------------------
 # [F]ind [P]ath
 # list directories in $PATH,press [enter] on an entry to list,press [escape] to go back,[escape] twice to exit completely
 
 fp() {
-	local loc
-	loc=$(echo "$PATH" | sed -e $'s/:/\\\n/g' |
-		eval "fzf ${FZF_DEFAULT_OPTS} --header='[find:path]'")
+  local loc
+  loc=$(echo "$PATH" | sed -e $'s/:/\\\n/g' |
+    eval "fzf ${FZF_DEFAULT_OPTS} --header='[find:path]'")
 
-	if [[ -d $loc ]]; then
-		rg --files "$loc" | rev | cut -d"/" -f1 | rev | eval \
-			"fzf ${FZF_DEFAULT_OPTS} --header='[find:exe] => ${loc}' \
-            >/dev/null"
-		fp
-	fi
+  if [[ -d $loc ]]; then
+    rg --files "$loc" | rev | cut -d"/" -f1 | rev | eval \
+      "fzf ${FZF_DEFAULT_OPTS} --header='[find:exe] => ${loc}' \
+      >/dev/null"
+    fp
+  fi
 }
 
 # [F]ind [FP]ath
 # list directories in $FPATH,press [enter] on an entry to list,press [escape] to go back,[escape] twice to exit completely
 ffp() {
-	local loc
-	loc=$(echo "$FPATH" | sed -e $'s/:/\\\n/g' |
-		eval "fzf ${FZF_DEFAULT_OPTS} --header='[find:path]'")
+  local loc
+  loc=$(echo "$FPATH" | sed -e $'s/:/\\\n/g' |
+    eval "fzf ${FZF_DEFAULT_OPTS} --header='[find:path]'")
 
-	if [[ -d $loc ]]; then
-		rg --files "$loc" | rev | cut -d"/" -f1 | rev | eval \
-			"fzf ${FZF_DEFAULT_OPTS} --header='[find:exe] => ${loc}' \
-            >/dev/null"
-		fp
-	fi
+  if [[ -d $loc ]]; then
+    rg --files "$loc" | rev | cut -d"/" -f1 | rev | eval \
+      "fzf ${FZF_DEFAULT_OPTS} --header='[find:exe] => ${loc}' \
+      >/dev/null"
+    fp
+  fi
 }
 
 # PROCESS
@@ -165,39 +165,39 @@ ffp() {
 # or press [escape] to go back to the process list. Press [escape] twice to exit completely.
 
 kp() {
-	local pid
-	pid=$(ps -ef | sed 1d | eval "fzf ${FZF_DEFAULT_OPTS} \
-        --header='[kill:process]'" | awk '{print $2}')
+  local pid
+  pid=$(ps -ef | sed 1d | eval "fzf ${FZF_DEFAULT_OPTS} \
+    --header='[kill:process]'" | awk '{print $2}')
 
-	if [ "x$pid" != "x" ]; then
-		echo "$pid" | xargs kill -"${1:-9}"
-		kp "$@"
-	fi
+  if [ "x$pid" != "x" ]; then
+    echo "$pid" | xargs kill -"${1:-9}"
+    kp "$@"
+  fi
 }
 
 # GitRebuild
 # ------------------
 grebuild() {
-	for BR in $(
-		git branch --format="%(refname:lstrip=2)"
-	); do
-		git checkout "$BR"
-		git checkout --orphan "${BR}_temp"
-		git add -A
-		git commit -m "Initial commit"
-		git branch -D "$BR"
-		git branch -m "$BR"
-	done
-	git gc --aggressive --prune=all
-	# git push -f --all
+  for BR in $(
+    git branch --format="%(refname:lstrip=2)"
+  ); do
+    git checkout "$BR"
+    git checkout --orphan "${BR}_temp"
+    git add -A
+    git commit -m "Initial commit"
+    git branch -D "$BR"
+    git branch -m "$BR"
+  done
+  git gc --aggressive --prune=all
+  # git push -f --all
 }
 
 # Remove cache
 # -------------------
 zr() {
-	rm -f ~/.cache/zsh/.zcompdump
-	rm -f ~/.cache/zsh/.zcompdump.zwc
-	# zplug clear
+  rm -f ~/.cache/zsh/.zcompdump
+  rm -f ~/.cache/zsh/.zcompdump.zwc
+  # zplug clear
 }
 
 # Create a folder and move into it in one command
