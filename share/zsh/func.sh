46c75a804e48224d13e3dca4038eece958c53edd
liuyinz <cheunghsu@outlook.com>
2021-04-28 19:00:36 +0800

    feat : add grebuild

--- share/zsh/func.sh
+++ share/zsh/func.sh
@@ -3,3 +3,20 @@
 # Create a folder and move into it in one command
 # -------------------
 mcd() { mkdir -p "$@" && cd "$_" || return; }
+
+# GitRebuild
+# ------------------
+grebuild() {
+  for BR in $(
+    git branch --format="%(refname:lstrip=2)"
+  ); do
+    git checkout "$BR"
+    git checkout --orphan "${BR}_temp"
+    git add -A
+    git commit -m "Initial commit"
+    git branch -D "$BR"
+    git branch -m "$BR"
+  done
+  git gc --aggressive --prune=all
+  # git push -f --all
+}
