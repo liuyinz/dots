0f475f3a168a9ed3ed6cbe68406f3254d296a222
liuyinz <cheunghsu@outlook.com>
2021-04-29 14:49:42 +0800

    fix : add two brew function

--- share/zsh/func.sh
+++ share/zsh/func.sh
@@ -1,5 +1,36 @@
 #!/usr/bin/env bash
 
+
+# brew remove useless dependence
+# -------------------
+# [B]rew [R]emove [D]ependence
+brd() {
+  export HOMEBREW_NO_AUTO_UPDATE=1
+  brew bundle dump -q -f --file="/tmp/Brewfile"
+  brew bundle -f --cleanup --file="/tmp/Brewfile"
+  rm /tmp/Brewfile
+}
+
+# [B]rew [I]nstall [O]lder Version Formula
+# -------------------
+bio() {
+  local pwd
+  pwd=$(pwd)
+  cd $HOMEBREW_FORMULA || return
+  if git cat-file -e $2 2>/dev/null; then
+    if [ -e $1.rb ]; then
+      echo "Installing..."
+      git checkout $2 $1.rb
+      HOMEBREW_NO_AUTO_UPDATE=1 brew install $1
+    else
+      echo "Error ! file $1.rb not exists."
+    fi
+  else
+    echo "Error ! Commit $2 not exists."
+  fi
+  cd $pwd || exit
+}
+
 # Remove .DS_Store files recursively in a directory, default .
 # ------------------
 function rmdsstore() {
